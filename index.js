7ec24989b00e592fbc33709ef1bb6b0c79646384
Erwan Bataille <erwan.bataille2910@gmail.com>
2024-03-19 12:13:31 +0100

    atelier modules chalk

---
+++ index.js
@@ -0,0 +1,8 @@
+import chalk from "chalk";
+
+const studentNameOne = "Mickeal";
+const studentNameTow = "Antoine";
+
+
+console.log(chalk.green(studentNameOne));
+console.log(chalk.red(studentNameTow));
